7cfa976947dd45badf650a2c23373adc972841c5
ADAPALA HARISH <adapalaharish@yahoo.com>
2023-03-21 16:53:13 +0530

    branch1

--- GET-vpc.sh
+++ GET-vpc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 REGION='us-east-1'
 FORMAT='json'
-aws ec2 describe-vpcs --region $REGION --output $FORMAT | "[].Vpcid"
+aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq "[].Vpcid"
