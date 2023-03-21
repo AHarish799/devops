c12cb533762412229ad936365241ec45478de7d0
ADAPALA HARISH <adapalaharish@yahoo.com>
2023-03-21 17:57:33 +0530

    hai

--- Effective-GetVpc.sh
+++ Effective-GetVpc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-RIGIONS=$@
-for REGION in ${RIGIONS}; do
+REGIONS=$@
+for REGION in ${REGIONS}; do
     echo "lets get the vpc id for Region $REGION"
-    aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq ".Vpcs[].Vpcid"
+    aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq ".Vpcs[].VpcId"
     echo '------------------------------------------------------------------------------'
 done
