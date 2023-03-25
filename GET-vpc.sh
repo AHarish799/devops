7316b99d1045ec0ed19f1c38a67491f331c03714
ADAPALA HARISH <adapalaharish@yahoo.com>
2023-03-25 23:27:48 +0530

    hai

--- GET-vpc.sh
+++ GET-vpc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REGION=$1
+REGION=us-east-1
 FORMAT='json'
 echo "Lets get the vpc details from The Region "
 aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq ".Vpcs[].Vpcid"
