65bc4db2ec9acd07820a402fbe56a6b4ec6d8731
ADAPALA HARISH <adapalaharish@yahoo.com>
2023-04-15 22:26:28 +0530

    added subnets

--- Effective-GetVpc.sh
+++ Effective-GetVpc.sh
@@ -3,6 +3,6 @@ REGIONS=$@
 for REGION in ${REGIONS};
  do
     echo "lets get the vpc id for Region $REGION"
-    aws ec2 describe-subnets --region $REGION --output json | jq ".Subnets[] | .VpcId, .SubnetId"
+    aws ec2 describe-subnets --region $REGION --output json | jq ".Subnets[] | .VpcId, .SubnetId, .OwnerId"
     echo '------------------------------------------------------------------------------'
 done
