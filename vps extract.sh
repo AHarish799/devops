e716988f610ae59b55f40d79d15b4101014cf50a
ADAPALA HARISH <adapalaharish@yahoo.com>
2023-04-16 01:40:41 +0530

    added vpc data

---
+++ vps extract.sh
@@ -0,0 +1,73 @@
+{
+  "Vpcs": [
+    {
+      "State": "available",
+      "CidrBlock": "10.24.0.0/16",
+      "Tags": [
+        {
+          "Value": "BETA-VPC",
+          "Key": "Name"
+        }
+      ],
+      "DhcpOptionsId": "dopt-0e9dd159b3640e873",
+      "CidrBlockAssociationSet": [
+        {
+          "AssociationId": "vpc-cidr-assoc-0fa3508d4b81cc101",
+          "CidrBlock": "10.24.0.0/16",
+          "CidrBlockState": {
+            "State": "associated"
+          }
+        }
+      ],
+      "VpcId": "vpc-0ad51df7a6df823e7",
+      "IsDefault": false,
+      "InstanceTenancy": "default"
+    },
+    {
+      "State": "available",
+      "CidrBlock": "10.34.0.0/16",
+      "Tags": [
+        {
+          "Value": "ALPHA-VPC",
+          "Key": "Name"
+        }
+      ],
+      "DhcpOptionsId": "dopt-0e9dd159b3640e873",
+      "CidrBlockAssociationSet": [
+        {
+          "AssociationId": "vpc-cidr-assoc-07258595963dab6ca",
+          "CidrBlock": "10.34.0.0/16",
+          "CidrBlockState": {
+            "State": "associated"
+          }
+        }
+      ],
+      "VpcId": "vpc-06972c9525dea54c8",
+      "IsDefault": false,
+      "InstanceTenancy": "default"
+    },
+    {
+      "State": "available",
+      "CidrBlock": "10.44.0.0/16",
+      "Tags": [
+        {
+          "Value": "AWSB44-VPC",
+          "Key": "Name"
+        }
+      ],
+      "DhcpOptionsId": "dopt-0e9dd159b3640e873",
+      "CidrBlockAssociationSet": [
+        {
+          "AssociationId": "vpc-cidr-assoc-0690c7adb5d0c0f68",
+          "CidrBlock": "10.44.0.0/16",
+          "CidrBlockState": {
+            "State": "associated"
+          }
+        }
+      ],
+      "VpcId": "vpc-005e6333e6eebe265",
+      "IsDefault": false,
+      "InstanceTenancy": "default"
+    }
+  ]
+}
