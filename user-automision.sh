a87ddb4704e83419b3fe9cf5de342165953bca8d
ADAPALA HARISH <adapalaharish@yahoo.com>
2023-04-19 23:05:28 +0530

    added user-sutomission

--- user-automision.sh
+++ user-automision.sh
@@ -11,4 +11,10 @@ else
             sudo touch /home/$USER/.ssh/authorized_keys
             sudo usermod -aG sudo $USER
             echo '$USER ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers
+            echo "Generating Random Password..."
+            SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | tail -1)
+            PASS="Harish@${RANDOM}${SPEC}"
+            echo "$USER:$PASS" | chpasswd
+            echo "Login Username Is $USER Password is $PASS"
+            passwd -e $USER 
 fi
